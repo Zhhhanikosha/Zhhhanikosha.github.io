f5a974f3dc4c53cf2fd3671c466971215421dcb2
Zhhhanikosha <123709694+Zhhhanikosha@users.noreply.github.com>
2023-03-30 20:47:42 +0600

    Delete sys_favorites.sql

--- database/sys_favorites.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `favorites` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `ep_id` int NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `episode_idx` (`ep_id`),
-  CONSTRAINT `episode` FOREIGN KEY (`ep_id`) REFERENCES `episodes` (`id`)
-) 
