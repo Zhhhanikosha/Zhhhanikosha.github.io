b24008033ddd4dcb0226aec7099f6a481519079f
Zhhhanikosha <123709694+Zhhhanikosha@users.noreply.github.com>
2023-03-30 20:47:35 +0600

    Delete sys_episodes.sql

--- database/sys_episodes.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `episodes` (
-  `id` int NOT NULL,
-  `ep_num` int DEFAULT NULL,
-  `duration` float DEFAULT NULL,
-  `upload_date` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) 
