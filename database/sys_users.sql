27f5876b1deab03cc11205f9b5b64297d011ef43
Zhhhanikosha <123709694+Zhhhanikosha@users.noreply.github.com>
2023-03-30 20:47:56 +0600

    Delete sys_users.sql

--- database/sys_users.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `users` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(45) DEFAULT NULL,
-  `last_name` varchar(45) DEFAULT NULL,
-  `favorite` int DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `favorite_idx` (`favorite`),
-  CONSTRAINT `favorite` FOREIGN KEY (`favorite`) REFERENCES `favorites` (`id`)
-)
