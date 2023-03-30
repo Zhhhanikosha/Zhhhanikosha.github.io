9018ddf2fbeeb36ff570d9681c3ba0b59aa25d4e
Zhhhanikosha <123709694+Zhhhanikosha@users.noreply.github.com>
2023-03-30 20:47:49 +0600

    Delete sys_seasons.sql

--- database/sys_seasons.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `seasons` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `season_num` int NOT NULL,
-  `ep_quantity` int NOT NULL,
-  `episode` int NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `episode_idx` (`episode`),
-  CONSTRAINT `episodes` FOREIGN KEY (`episode`) REFERENCES `episodes` (`id`)
-) 
